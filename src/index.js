8a420ce5083d8cad985e52c9f48fa1634d4a77b4
Md Adil <md-adil@live.com>
2023-01-01 13:33:56 +0530

    initial calculator

---
+++ src/index.js
@@ -0,0 +1,71 @@
+const output = document.getElementById("output");
+let isOn = false;
+function handleClick(e) {
+    const out = document.getElementById("output");
+    const val = e.target.value;
+    if (val === "ON") {
+        e.target.value = "OFF";
+        e.target.innerText = "OFF";
+        output.value = "0";
+        isOn = true;
+        return;
+    }
+    if (val === "OFF") {
+        e.target.value = "ON";
+        e.target.innerText = "ON";
+        isOn = false;
+        output.value = "";
+        return;
+    }
+
+    if (!isOn) {
+        return;
+    }
+    switch (val) {
+        case "<-":
+            handleBack();
+            break;
+        case "=":
+            handleOperation();
+            break;
+        default:
+            if (out.value === "0") {
+                output.value = "";
+            }
+            out.value = out.value + val;
+    }
+}
+
+function handleBack() {
+    const val = output.value;
+    output.value = output.value.substring(0, val.length - 1);
+}
+function handleOperation() {
+    out.value = eval(out.value);
+}
+
+const symbols = ["+", "-", "*", "/", "%", "<-", "="];
+
+function addButtons(items) {
+    const np = document.getElementById("numpad");
+    for (const num of items) {
+        const btn = document.createElement("button");
+        btn.innerText = num;
+        btn.className = "p-4 bg-gray-600 border border-solid border-200 hover:bg-gray-700";
+        np.appendChild(btn);
+        btn.value = num;
+        btn.addEventListener("click", handleClick);
+    }
+}
+
+function numpads() {
+    addButtons(["ON", "CE"]);
+    addButtons([...new Array(10).keys()].reverse());
+    addButtons(symbols);
+}
+
+function main() {
+    numpads();
+}
+
+main();
